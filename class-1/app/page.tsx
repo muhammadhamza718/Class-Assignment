19d352d232b6796fe69e33721bcf83ae950da7e6
muhammadhamza718 <160007582+muhammadhamza718@users.noreply.github.com>
2024-10-04 17:25:00 +0500

    Add files via upload

---
+++ class-1/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function homepage() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <br />
+      <h2>from class 1</h2>
+    </>
+  );
+}
+export default homepage;
